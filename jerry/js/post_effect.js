d4ea40da28df67bf90ccdf4c25bffba3e70ffe6e
Jerry-the-potato <gery19735@gmail.com>
2021-09-17 11:07:43 +0800

    Add files via upload
    update article

--- jerry/js/post_effect.js
+++ jerry/js/post_effect.js
@@ -8,8 +8,9 @@
         const p = article.getElementsByTagName("p")[0];
         const p_CSS = window.getComputedStyle(p);
         let SetHeight = () => {
-            const P_height = p_CSS.height.substring(0, p_CSS.height.lastIndexOf("p") - 1);
-            const p_lineHeight = p_CSS.lineHeight.substring(0, p_CSS.lineHeight.lastIndexOf("p") - 1);
+            const P_height = p_CSS.height.substring(0, p_CSS.height.lastIndexOf("p"));
+            const p_lineHeight = p_CSS.lineHeight.substring(0, p_CSS.lineHeight.lastIndexOf("p"));
+            console.log(P_height);
             if(P_height > p_lineHeight * 5){
                 if(more.textContent == "Read More"){
                         article.style.maxHeight = p_CSS.height;
