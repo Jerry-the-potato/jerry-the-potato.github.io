67e6209965da24deccfc8407962563512fa919af
Jerry-the-potato <gery19735@gmail.com>
2021-09-24 18:33:19 +0800

    Add files via upload
    binary tree classes version

---
+++ Chapter3-demo-class/js/main.js
@@ -0,0 +1,47 @@
+
+// main.js處理動畫模組
+AnimationLoop();
+function AnimationLoop(){
+    try{
+        Resize("#game-box", canvas, context, '#000');
+        Redraw();
+    }catch(e){
+        window.myErrorMessage = e.message;
+    }
+    requestAnimationFrame(AnimationLoop);
+}
+function Resize(boxID, canvas, context, fillStyle=undefined){
+    if(WIDTH != window.innerWidth * RATIO || HEIGHT != window.innerHeight * RATIO){
+        WIDTH = window.innerWidth * RATIO;
+        HEIGHT = window.innerHeight * RATIO;
+        let box = document.querySelector(boxID);
+        canvas.width = WIDTH;
+        canvas.height = HEIGHT;
+        canvas.style.width = WIDTH/RATIO + "px";
+        canvas.style.height = HEIGHT/RATIO + "px";
+        box.style.width = WIDTH/RATIO + "px";
+        box.style.height = HEIGHT/RATIO + "px";
+        if(fillStyle != undefined){
+            context.beginPath();
+            context.rect(0, 0, WIDTH, HEIGHT);
+            context.fillStyle = fillStyle;
+            context.fill();
+        }
+    }
+}
+function Redraw(){
+    Clear(context);
+    MouseAnime();
+}
+function Clear(context){
+    // context.clearRect(0, 0, WIDTH, HEIGHT);
+    context.beginPath();
+    context.rect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = 'rgba(0, 0, 0, ' + alpha + ')';
+    context.fill();
+}
+function MouseAnime(){
+    myMouse.NextFrame();
+    myTree.Transform();
+    myTree.Draw();
+}
